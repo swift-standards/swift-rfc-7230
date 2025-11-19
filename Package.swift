7fafeb6e0eeab2be8b5682c6e42b6398de5e7aa2
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-11-19 10:05:50 +0100

    Update gitignore and Package.swift

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-collections", from: "1.0.3"),
-        .package(path: "../swift-rfc-3986"),
-        .package(path: "../swift-rfc-7231")
+        .package(url: "https://github.com/swift-standards/swift-rfc-3986", from: "0.1.0"),
+        .package(url: "https://github.com/swift-standards/swift-rfc-7231", from: "0.1.0")
     ],
     targets: [
         .target(
